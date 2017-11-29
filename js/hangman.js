308a22af9ee472016c0b8264fd40784b50cb1f90
Garred Buehl Murphy <gmurphy@csumb.edu>
2017-11-29 12:39:02 +0000

    finally fixed everything

--- js/hangman.js
+++ js/hangman.js
@@ -1,3 +1,145 @@
+            var selectedWord = "";
+            var selectedHint = "";
+            var board = "";
+            var remainingGuesses = 6;
+            var words = [{ word: "snake", hint: "It's a reptile"}, { word: "monkey", hint: "It's a mammal"}, { word: "beetle", hint: "It's an insect"}];
+            //var words = ["snake","monkey","beetle"];
+
+            
+
+            window.onload = startGame;
+            
+            
+            
+            
+            
+            
+            
+            function startGame(){
+    pickWord();
+    initBoard();
+    updateBoard();
+    createLetters();
+    
+    $(".replayBtn").on("click", function () {
+        location.reload();
+    });
+}
+            function pickWord() {
+                var randomInt = Math.floor(Math.random() * words.length);
+                selectedWord = words[randomInt].word.toUpperCase();
+                selectedHint = words[randomInt].hint;
+
+                
+                console.log (selectedWord);
+            }
+            
+            function updateBoard()
+            {
+                $("#word").empty();
+                for (var letter of board)
+            {
+
+                //document.getElementById("word").innerHTML += letter + " ";
+                $("#word").append(letter + " ")
+            }
+            
+            $("#hintBtn").on("click", function() {
+                $("#hint").empty();
+                $("#hint").append("<br />");
+                $("#hint").append("<span class ='hint'>Hint: " + selectedHint + "</span>");  
+            });
+            
+            
+            }
+            
+            function initBoard()
+            {
+                for (var letter in selectedWord)
+                {
+                    board += '_';
+                }
+            }
+            
+    function createLetters() {
+        
+        var alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+    for (var letter of alphabet) {
+        $("#letters").append("<button class='letter' id='" + letter +"'>" + letter+"</button>");
+    }
+        $(".letter").click(function(){
+            //console.log($(this).attr("id"));
+            checkLetter($(this).attr("id"));
+            disableButton($(this));
+        });
+    
+}
+
+function checkLetter(letter) {
+    var positions = new Array();
+    
+    for (var i = 0; i < selectedWord.length; i++) {
+        //console.log(selectedWord);
+        if (letter == selectedWord[i]) {
+            positions.push(i);
+        }
+    }
+    
+    if (positions.length > 0) {
+        updateWord(positions, letter);
+        
+        if(!board.includes('_')){
+            endGame(true);
+        }
+    } else {
+        remainingGuesses -= 1;
+        updateMan();
+    }
+    
+    if (remainingGuesses <= 0){
+        endGame(false);
+    }
+    
+    //$(".replayBtn").on("click", function() {
+        //location.reload();
+    //});
+}
+
+function updateWord(positions, letter) {
+    
+    for (var pos of positions) {
+        board = replaceAt(board, pos, letter);
+    }
+    updateBoard();
+}
+
+function replaceAt(str, index, value) {
+    return str.substr(0, index) + value + str.substr(index + value.length);
+}
+
+function updateMan() {
+    $("#hangImg").attr("src", "img/stick_" + (6 - remainingGuesses) + ".png");
+}
+
+function endGame(win) {
+    $("#letters").hide();
+    
+    if (win) {
+        $('#won').show();
+        $("#hint").empty();
+    }
+    else
+    {
+        $('#lost').show();
+    }
+}
+
+function disableButton(btn)
+{
+    btn.prop("disabled", true);
+    btn.attr("class", "btn btn-danger");
+}
+/*
 var selectedWord = "";
 var selectedHint = "";
 var board = "";
@@ -88,27 +230,15 @@ function checkLetter(letter) {
         location.reload();
     });
 }
-
-
-
-
-
-
-
-
-
 $("#letterBtn").click(function(){
                 var boxVal = $("#letterBox").val();
                 console.log("You pressed the button and it had the value: " + boxVal);
                 })
-
-
-
 function replaceAt(str, index, value) {
     return str.substr(0,index) + value + str.substr(index + value.length);
 }
 
-function updateMain() {
+function updateMan() {
     $("#hangImg").attr("src", "img/stick_" + (6 - remainingGuesses) + ".png");
 }
 
@@ -123,3 +253,4 @@ function endGame(win) {
         $('#lost').show();
     }
 }
+*/
